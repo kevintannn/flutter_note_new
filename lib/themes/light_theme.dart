5d9ae22bfcad3240a63653f705afb96452531e56
Kevin Tan <48116781+kevintannn@users.noreply.github.com>
2024-07-06 15:10:16 +0700

    configure theme

---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+        surface: Colors.grey.shade300,
+        primary: Colors.grey.shade500,
+        secondary: Colors.grey.shade200,
+        tertiary: Colors.white,
+        inversePrimary: Colors.grey.shade900),
+    splashColor: Colors.transparent,
+    textTheme: ThemeData.light()
+        .textTheme
+        .apply(bodyColor: Colors.grey[800], displayColor: Colors.black));
