1f3ab58bc1716f9dab17b1da6b0b559bbaa6f38b
Kevin Tan <48116781+kevintannn@users.noreply.github.com>
2024-07-06 15:10:52 +0700

    create photos page

---
+++ lib/pages/photos_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PhotosPage extends StatelessWidget {
+  const PhotosPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
