c5c3c2785ab63285af2dcf43fe878cdf4f291c87
Kevin Tan <48116781+kevintannn@users.noreply.github.com>
2024-07-12 12:42:48 +0700

    add shared preferences and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_note/components/my_loading_spinner.dart';
 import 'package:my_note/firebase_options.dart';
 import 'package:my_note/pages/router_page.dart';
-import 'package:my_note/themes/dark_theme.dart';
-import 'package:my_note/themes/light_theme.dart';
+import 'package:my_note/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,11 +18,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      home: const RouterPage(),
+    return FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ChangeNotifierProvider(
+            create: (context) => ThemeProvider(),
+            child: Consumer<ThemeProvider>(
+              builder: (context, themeProvider, child) {
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  theme: themeProvider.currentTheme,
+                  home: const RouterPage(),
+                );
+              },
+            ),
+          );
+        } else {
+          return const MyLoadingSpinner();
+        }
+      },
     );
   }
 }
